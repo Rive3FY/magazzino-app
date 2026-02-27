ce8b1bbda180ea692f170c7c31ca726b52ac85ec
Gianluca <gianluca.rivetti2000@gmail.com>
2026-02-27 22:22:51 +0100

    add middleware auth

--- app/giacenze/page.tsx
+++ app/giacenze/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useMemo, useState } from "react";
 import * as XLSX from "xlsx";
 import { createClient } from "../_lib/supabase/client";
